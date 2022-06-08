0c83ddb54ae371f98ce57aca90a745de4928bc81
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-08 14:09:01 +0300

    commit tree 3fba2bb1ef09af4783a612a47915e25edff719f5

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function receivesAFunction(callback) {
+    return callback();
+}
+
+function returnsANamedFunction () {
+    return returnsANamedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function(){};
+}
